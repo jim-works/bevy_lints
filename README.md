00a9d9c2ead0280ddb2f95b4b4675f92f049e8d4
Jim <jim3moore@protonmail.com>
2024-12-31 08:33:18 -0500

    Create StateScopedEntities lint

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # bevy_lints
 
-This project provides my lints for bevy, using [Dylint](https://github.com/trailofbits/dylint). This is mainly for my other project, [Wisphaven](https://github.com/jim-works/Wisphaven)
+This project provides my lints for bevy, using [Dylint](https://github.com/trailofbits/dylint). This is mainly for my other project, [Wisphaven](https://github.com/jim-works/Wisphaven). These lints will be specialized for what I care about, so you may find some annoying. You can always pick which lints to use.
+
+Current lints:
+- Require all spawned entities to be state scoped
 
 # How to use
 
-You do not need to clone this repository to use it. Dylint provides support for running lints from git repos, though you can also clone and run it from a path on your system. See https://github.com/trailofbits/dylint#workspace-metadata for instructions.
+You do not need to clone this repository to use it. Dylint provides support for running lints from git repos, though you can also clone and run it from a path on your system if you prefer. See https://github.com/trailofbits/dylint#workspace-metadata for more detailed instructions (including how to use a subset of lints).
 
 1. Install the necessary dependencies by running
 
@@ -16,11 +19,30 @@ You do not need to clone this repository to use it. Dylint provides support for 
 
 [workspace.metadata.dylint]
 libraries = [
-    { git = "https://github.com/jim-works/bevy_lints", branch="main" },
+    { git = "https://github.com/jim-works/bevy_lints", branch="master" },
+    # or if you have cloned locally
+    # { path = "path/to/repo/folder" }
 ]
 ```
 
 3. Now, you can run `cargo dylint --all` in your project to get the lints.
+
+## Disabling lints
+
+You can use something like `#[cfg_attr(dylint_lib = "bevy_lints", allow(state_scoped_entities))]` to disable a specific lint.
+You may get a warning about an unknown library, which can be avoided by updating your Cargo.toml as described [here.](https://github.com/trailofbits/dylint?tab=readme-ov-file#rustcs-unexpected_cfg-lint)
+
+If you don't care about receiving warnings about unknown lints, you can also use `#[allow(lint_name)]` in combination with `#[allow(unknown_lints)]`. If you go this path, you would probably want to use the global version `#![allow(unknown_lints)]` in your crate's `lib.rs` or `mod.rs`. Another approch is to add this to your `Cargo.toml`:
+
+```toml
+
+[lints.rust] #or [workspace.lints.rust]
+unknown_lints = "allow"
+```
+
+## VSCode integration
+
+See [the Dylint docs](https://github.com/trailofbits/dylint?tab=readme-ov-file#vs-code-integration).
 
 # Related projects
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
 #![feature(rustc_private)]
-#![feature(let_chains)]
 #![warn(unused_extern_crates)]
 
-extern crate rustc_arena;
-extern crate rustc_ast;
-extern crate rustc_ast_pretty;
-extern crate rustc_data_structures;
+// extern crate rustc_arena;
+// extern crate rustc_ast;
+// extern crate rustc_ast_pretty;
+// extern crate rustc_data_structures;
 extern crate rustc_errors;
 extern crate rustc_hir;
-extern crate rustc_hir_pretty;
-extern crate rustc_index;
-extern crate rustc_infer;
-extern crate rustc_lexer;
+// extern crate rustc_hir_pretty;
+// extern crate rustc_index;
+// extern crate rustc_infer;
+// extern crate rustc_lexer;
 extern crate rustc_middle;
-extern crate rustc_mir_dataflow;
-extern crate rustc_parse;
+// extern crate rustc_mir_dataflow;
+// extern crate rustc_parse;
 extern crate rustc_span;
-extern crate rustc_target;
-extern crate rustc_trait_selection;
+// extern crate rustc_target;
+// extern crate rustc_trait_selection;
 
-use rustc_lint::LateLintPass;
+use clippy_utils::{diagnostics::span_lint_and_help, is_diag_trait_item, source::snippet};
+use rustc_errors::Applicability;
+use rustc_hir::{Expr, ExprKind};
+use rustc_lint::{LateContext, LateLintPass};
+use rustc_middle::lint::in_external_macro;
+use rustc_span::Symbol;
 
 dylint_linting::declare_late_lint! {
     /// ### What it does
@@ -39,12 +43,77 @@ dylint_linting::declare_late_lint! {
     /// ```
     pub STATE_SCOPED_ENTITIES,
     Warn,
-    "description goes here"
+    "checks for spawning entities without a state scope"
+}
+
+impl StateScopedEntities {
+    //returns true if
+    fn argument_contains_state_scope<'tcx>(ctx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) -> bool {
+        if let ExprKind::Tup(items) = expr.kind {
+            return items
+                .iter()
+                .any(|item| Self::argument_contains_state_scope(ctx, item));
+        }
+        let rustc_middle::ty::TyKind::Adt(adt, _) =
+            ctx.typeck_results().expr_ty(expr).peel_refs().kind()
+        else {
+            return false;
+        };
+
+        let Some(variant) = adt.variants().iter().next() else {
+            return false;
+        };
+
+        variant.ident(ctx.tcx).name == Symbol::intern("StateScoped")
+    }
 }
 
 impl<'tcx> LateLintPass<'tcx> for StateScopedEntities {
     // A list of things you might check can be found here:
     // https://doc.rust-lang.org/stable/nightly-rustc/rustc_lint/trait.LateLintPass.html
+
+    fn check_expr(&mut self, ctx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
+        if in_external_macro(ctx.tcx.sess, expr.span) {
+            return;
+        }
+
+        let ExprKind::MethodCall(segment, object, func_args, expr_span) = expr.kind else {
+            return;
+        };
+
+        if segment.ident.name != Symbol::intern("spawn") {
+            return;
+        }
+
+        let rustc_middle::ty::TyKind::Adt(adt, _) =
+            ctx.typeck_results().expr_ty(object).peel_refs().kind()
+        else {
+            return;
+        };
+
+        let Some(variant) = adt.variants().iter().next() else {
+            return;
+        };
+
+        //only need to check for .spawn() on Commands and not ChildBuilder since the despawn is recursive
+        if variant.ident(ctx.tcx).name != Symbol::intern("Commands") {
+            return;
+        }
+
+        if !func_args
+            .iter()
+            .any(|arg| StateScopedEntities::argument_contains_state_scope(ctx, arg))
+        {
+            span_lint_and_help(
+                ctx,
+                STATE_SCOPED_ENTITIES,
+                expr_span,
+                "Commands::spawn() call is missing StateScoped component",
+                None,
+                "add a StateScoped() component",
+            );
+        }
+    }
 }
 
 #[test]
